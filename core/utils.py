a04b351030925aec751b4be37392997142edc1c5
fpemud <fpemud@sina.com>
2014-12-01 10:54:37 +0800

    support for split-file dmovie element, WIP

--- core/utils.py
+++ core/utils.py
@@ -244,6 +244,10 @@ class Utils(QObject):
 
         allFiles = [os.path.basename(x) for x in allFiles]
         allMatches = (longest_match(x, os.path.basename(name)) for x in allFiles)
+        matchesFilter = lambda x: x and x != os.path.basename(name) \ 
+                                    and not x[0] in "0123456789" \ 
+                                    and (len(x) > 5 or any(map(lambda ch: ch in x, sep_chars))) 
+
         matchesFilter = lambda x: x and x != os.path.basename(name) and (len(x) > 5 or any(map(lambda ch: ch in x, sep_chars)))
         filteredMatches = filter(matchesFilter, allMatches)
         nameFilter = min(filteredMatches, key=len) if filteredMatches else ""
